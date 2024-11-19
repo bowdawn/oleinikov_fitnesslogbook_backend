2bc8aa65285b54d6a14bbdfdaab8c3a8a2dc44d6
Bogdan Oleinikov <boleinikov@gmail.com>
2024-11-19 15:30:30 -0500

    add app

--- oleinikov_fitnesslogbook_backend/wsgi.py
+++ oleinikov_fitnesslogbook_backend/wsgi.py
@@ -4,3 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oleinikov_fitnesslogbook_backend.settings')
 
 application = get_wsgi_application()
+app = application 
